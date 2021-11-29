583b8e4033ea04692dd98e3f0c873297836477ca
BirdLQ <toutlemonder@gmail.com>
2021-11-29 01:14:46 +0100

    Update turtle.cock.py

--- turtle.cock.py
+++ turtle.cock.py
@@ -3,7 +3,7 @@
 @author: BirdLQ
 """
 
-import turtle, time
+import turtle
 
 heure_=int(input("Enter the time in hh format: "))
 minute_=int(input("Enter the minutes in mm format: "))
@@ -14,25 +14,23 @@ def formt(h, m, s):
     minute, seconde = divmod(s, 60)
     heure, minute = divmod(minute+m, 60)
     heure = (heure+h)%12
+    
     return heure, minute, seconde
 
-heure, minute, seconde = formt(heure_, minute_, seconde_)
-
 #The time is entered by the user
-def write_time(heure, minute, seconde):
+def write_time(heure, minute, seconde):    
+    t.undo()
     t.goto(-75,-250)
-    if heure_ > 12:
-        t.write("It's : {:02d}:{:02d}:{:02d} pm".format(heure, minute, seconde),
-                 font=("Arial", 16, "normal"))
-    else:
-        t.write("It's : {:02d}:{:02d}:{:02d} am".format(heure, minute, seconde),
-                 font=("Arial", 16, "normal"))
+    
+    t.write("It's : {:02d}:{:02d}:{:02d} {moment}".format(heure, minute, seconde, 
+            moment = "pm" if heure_ > 12 else "am"), font=("Arial", 16, "normal"))
 
 #fonction to calculate hands angles
 def angle(h, m, s):
-    angle_s=(360 / 60) * s
-    angle_m=(360 / 60) * m + angle_s*1/60
-    angle_h=(360 / 12) * h + angle_m*1/12
+    angle_s= (360 / 60) * s
+    angle_m= (360 / 60) * m + angle_s/60
+    angle_h= (360 / 12) * h + angle_m/12
+    
     return angle_h, angle_m, angle_s
 
 #clock face
@@ -66,10 +64,9 @@ def horloge(x=0, y=0, c=200, e=180, p=20, e_e=185, p_p=15):
     None.
 
     '''
-    t.ht()
-    t.width(5)
     t.penup()
-    t.goto(x,y-c)
+    t.goto(x+c,y)
+    t.clear()
     t.pendown()
     t.circle(c)
     t.left(90)
@@ -111,12 +108,9 @@ def aigh(h, x=0, y=0, f=100):
     None.
 
     '''
-    hour_t.ht()
-    hour_t.goto(x,y)
-    hour_t.setheading(90)
-    hour_t.rt(h)
-    hour_t.pendown()
-    hour_t.width(10)
+    hour_t.goto(x, y)
+    hour_t.clear()
+    hour_t.setheading(h)
     hour_t.forward(f)
 
 def aigm(m, x=0, y=0, f=150):
@@ -137,12 +131,9 @@ def aigm(m, x=0, y=0, f=150):
     None.
 
     '''
-    min_t.ht()
-    min_t.goto(x,y)
-    min_t.setheading(90)
-    min_t.rt(m)
-    min_t.pendown()
-    min_t.width(5)
+    min_t.goto(x, y)
+    min_t.clear()
+    min_t.setheading(m)
     min_t.forward(f)
 
 def aigs(s, x=0, y=0, f=170):
@@ -163,64 +154,62 @@ def aigs(s, x=0, y=0, f=170):
     None.
 
     '''
-    sec_t.ht()
-    sec_t.goto(x,y)
-    sec_t.setheading(90)
-    sec_t.color("red")
-    sec_t.width()
-    sec_t.rt(s)
-    sec_t.pendown()
-    sec_t.width(1)
+    sec_t.goto(x, y)
+    sec_t.clear()
+    sec_t.setheading(s)
     sec_t.forward(f)
-
-#init turtle
-t = turtle.Turtle()
-
-#turtles for hands
-hour_t = turtle.Turtle()
-min_t = turtle.Turtle()
-sec_t = turtle.Turtle()
-
-wn = turtle.Screen()
-wn.title("Clock")
-
-wn.tracer(0)
-
-#draw the clock
-horloge()
 
 def draw(h, m, s):
     aigh(h)
     aigm(m)
     aigs(s)
-    
-def refresh(h=heure, m=minute, s=seconde):
-    global seconde
-    wn.tracer(0)
-    hour_t.undo()
-    min_t.undo()
-    sec_t.undo()
-    
-    h_, m_, s_ = formt(h, m, s)
-    t.undo()
-    write_time(h_, m_, s_)
-    
-    #get the hands angles
-    agl_h, agl_m, agl_s = angle(h_, m_, s_)
 
-    #draw the hands
+heure, minute, seconde = formt(heure_, minute_, seconde_)
+    
+def refresh():
+    global heure, minute, seconde
+    
+    heure, minute, seconde = formt(heure, minute, seconde)
+
+    write_time(heure, minute, seconde)
+    # get the hands angles
+    agl_h, agl_m, agl_s = angle(heure, minute, seconde)
+
+    # draw the hands
     draw(agl_h, agl_m, agl_s)
 
-    wn.update()
-    
-    seconde +=1
+    seconde += 1
 
-    #wn.ontimer(refresh, 1000)
+    screen.update()
 
-for _ in iter(int, 1):
-    refresh(s=seconde)
-    time.sleep(1)
+    screen.ontimer(refresh, 1000)
 
-wn.exitonclick()
+screen = turtle.Screen()
+screen.title("Clock")
+screen.mode('logo')  # 0 degrees at top, circles go clockwise, like a clock!
+screen.tracer(False)
 
-#wn.mainloop()
+# main turtle
+t = turtle.Turtle()
+t.hideturtle()
+t.width(5)
+
+horloge() #draw clock face
+
+# turtles for hands
+hour_t = turtle.Turtle()
+hour_t.hideturtle()
+hour_t.width(10)
+
+min_t = turtle.Turtle()
+min_t.hideturtle()
+min_t.width(5)
+
+sec_t = turtle.Turtle()
+sec_t.hideturtle()
+sec_t.width(1)
+sec_t.color("red")
+
+refresh()
+
+screen.mainloop()
